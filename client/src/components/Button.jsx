8faab902c553149f2813b4d86154cd60f583f342
abhay0031 <abhayjeet0031@gmail.com>
2025-04-12 11:37:42 +0530

    Updated Code part 1 started

--- client/src/components/Button.jsx
+++ client/src/components/Button.jsx
@@ -1,21 +1,52 @@
+
+import { useLocation } from 'react-router-dom'
 import React from 'react'
+import { Link, NavLink, useNavigate } from 'react-router-dom'
+import Button from '../components/Button'
+import { logout } from '../services/operations/AuthAPIs'
+import { useDispatch, useSelector } from 'react-redux'
 
-const Button = ({
-    type = 'button',
-    className = '',
-    disabled = false,
-    children,
-    active = true,
-    onClick = () => { }
-}) => {
+const DashboardLayout = ({ children }) => {
+
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+    const location = useLocation();
+    const { user } = useSelector(state => state.auth)
+
     return (
-        <button
-        onClick={onClick}
-            disabled={disabled}
-            className={`w-full ${active ? " bg-green-700 hover:bg-green-800 focus:bg-green-800" : " bg-red-700 hover:bg-red-800 focus:bg-red-800"} transiiton-all duration-300 py-1 px-3 rounded-lg text-sm ${className}`}
-            type={type}>{children}</button>
-
+        <section className=''>
+            <div className='flex py-3 px-3 justify-between items-center gap-y-5  my-3 text-lg bg-slate-900 rounded-lg border border-slate-600'>
+                <span className='space-x-1 md:space-x-3 text-sm md:text-base'>
+                    <NavLink to={"/dashboard"} className={`hover:bg-slate-700 transition-all duration-300 px-3 py-1 rounded-full ${location.pathname === "/dashboard" && "bg-slate-600"}`}>
+                        Profile
+                    </NavLink>
+                    {
+                        user.role === "admin" ? <>
+                            <Link to={"/dashboard/create-quiz"} className={`hover:bg-slate-700 transition-all duration-300 px-3 py-1 rounded-full ${location.pathname.includes("create") && "bg-slate-600"}`}>
+                                Create
+                            </Link>
+                            <Link to={"/dashboard/quizes"} className={`hover:bg-slate-700 transition-all duration-300 px-3 py-1 rounded-full ${location.pathname.includes("quizes") && "bg-slate-600"}`}>
+                                Quizes
+                            </Link>
+                            <Link to={"/dashboard/notes"} className={`hover:bg-slate-700 transition-all duration-300 px-3 py-1 rounded-full ${location.pathname.includes("notes") && "bg-slate-600"}`}>
+                                Notes
+                            </Link>
+                        </> : <>
+                            <Link to={"/dashboard/history"} className={`hover:bg-slate-700 transition-all duration-300 px-3 py-1 rounded-full ${location.pathname.includes("history") && "bg-slate-600"}`}>
+                                History
+                            </Link>
+                        </>
+                    }
+                </span>
+                <span>
+                    <Button active={false} onClick={() => logout(dispatch, navigate)}>
+                        Logout
+                    </Button>
+                </span>
+            </div>
+            {children}
+        </section>
     )
 }
-                
-export default Button
+
+export default DashboardLayout
