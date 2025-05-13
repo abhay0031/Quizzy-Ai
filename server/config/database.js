7ef1dd57a4b5020243c1d2756fb970a1ce5b4297
abhay0031 <abhayjeet0031@gmail.com>
2025-05-13 13:41:58 +0530

    ba

--- server/config/database.js
+++ server/config/database.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose")
 require("dotenv").config();
 
 exports.connectToDB = () => {
-    // mongoose.connect("mongodb+srv://Myusername:admin@mycluster.stkyauo.mongodb.net/quizzy?retryWrites=true&w=majority&appName=MyCluster",{})
+    mongoose.connect("mongodb+srv://Myusername:admin@mycluster.stkyauo.mongodb.net/quizzy?retryWrites=true&w=majority&appName=MyCluster",{})
     mongoose.connect("mongodb://localhost:27017/quizzy",{})
     .then(() => {
         console.log("Database connection successfull")
